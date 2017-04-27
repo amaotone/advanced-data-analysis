0b4dd73912d7b16e31b85074c78e0729d920a358
Amane Suzuki <amane.suzu@gmail.com>
2017-04-27 23:51:55 +0900

    Use scikit-learn utils

--- ada/kernels.py
+++ ada/kernels.py
@@ -1,22 +1,11 @@
 import numpy as np
-from scipy.spatial import distance_matrix
+from sklearn.metrics.pairwise import euclidean_distances
 
 
 def gaussian_kernel(h):
     def f(X, Y=None):
-        if Y is None:
-            Y = X
-        gamma = 1 / (2 * h ** 2)
-        return np.exp(-gamma * euclidean_distance(X, Y, squared=True))
+        K = euclidean_distances(X, Y, squared=True)
+        K *= -1 / (2 * h ** 2)
+        return np.exp(K, K)
     
     return f
-
-
-def euclidean_distance(X, Y=None, squared=False):
-    if Y is None:
-        Y = X
-    
-    if squared:
-        return np.power(distance_matrix(X, Y), 2)
-    else:
-        return distance_matrix(X, Y)
--- ada/regression/kernel_ridge.py
+++ ada/regression/kernel_ridge.py
@@ -4,24 +4,27 @@ from sklearn.base import BaseEstimator, RegressorMixin
 from ..kernels import gaussian_kernel
 
 
-class KernelRidgeRegression(BaseEstimator, RegressorMixin):
+class KernelRidge(BaseEstimator, RegressorMixin):
     def __init__(self, kernel=gaussian_kernel, h=0.1, l2=0.0):
         self.kernel = kernel
         self.h = h
         self.l2 = l2
+        
         self.theta = None
-        self.X_ = None
+        self.X_fit = None
     
     def fit(self, X, y):
         n_samples = X.shape[0]
         K = self._get_kernel(X)
         self.theta = np.linalg.solve(
-            K.dot(K) + self.l2 * np.eye(n_samples),
-            np.transpose(K).dot(y))
-        self.X_ = X
+            K.T.dot(K) + self.l2 * np.eye(n_samples),
+            K.T.dot(y))
+        self.X_fit = X
+        
+        return self
     
     def predict(self, X):
-        K = self._get_kernel(X, self.X_)
+        K = self._get_kernel(X, self.X_fit)
         return K.dot(self.theta)
     
     @property
