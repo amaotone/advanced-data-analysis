412c70285b6fc00c898844d4c399139c96b24d8f
Amane Suzuki <amane.suzu@gmail.com>
2017-04-28 18:16:38 +0900

    Add example

--- ada/regression/kernel_lasso.py
+++ ada/regression/kernel_lasso.py
@@ -1,19 +1,17 @@
 import numpy as np
 from sklearn.base import BaseEstimator, RegressorMixin
-from sklearn.metrics import mean_squared_error as MSE
 
 from ..kernels import gaussian_kernel
 
 
 class KernelLasso(BaseEstimator, RegressorMixin):
     def __init__(self, kernel=gaussian_kernel, h=0.1, l1=0.1,
-                 max_iter=1000000, eps=1e-5, check_conv=10000, verbose=False):
+                 max_iter=1000000, eps=1e-5, verbose=False):
         self.kernel = kernel
         self.h = h
         self.l1 = l1
         self.max_iter = max_iter
         self.eps = eps
-        self.check_conv = check_conv
         self.verbose = verbose
         
         self.theta = None
@@ -21,13 +19,13 @@ class KernelLasso(BaseEstimator, RegressorMixin):
     
     def fit(self, X, y):
         K = self._get_kernel(X)
-        theta, z, u = [np.random.rand(K.shape[0]) for _ in range(3)]
+        theta, z, u = [np.zeros((K.shape[0], 1)) for _ in range(3)]
         zeros = np.zeros(theta.shape)
         ones = np.ones(theta.shape)
         
         # pre-computation
         KTK = K.T.dot(K)
-        KTy = K.T.dot(y)
+        KTy = K.T.dot(y.reshape(-1, 1))
         Q = np.linalg.inv(KTK + np.eye(KTK.shape[0]))
         
         # alternating direction method of multipliers
@@ -38,13 +36,11 @@ class KernelLasso(BaseEstimator, RegressorMixin):
                 np.minimum(zeros, theta + u + self.l1 * ones)
             u += (theta - z)
             
-            # lazy convergence check
-            if i % self.check_conv == 0:
-                error = MSE(theta, theta_old)
-                if self.verbose:
-                    print(i, error)
-                if error < self.eps:
-                    break
+            error = np.linalg.norm(theta - theta_old)
+            if self.verbose:
+                print(i, error)
+            if error < self.eps:
+                break
         else:
             raise RuntimeError("ADMM didn't converge.")
         
